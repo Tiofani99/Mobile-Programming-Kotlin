6e2765a718a4d81e74a641448fd43351e0433b8a
Tiofani99 <tiofani03@gmail.com>
2020-03-13 21:29:27 +0700

    App menghitung volume balok sederhana

---
+++ barVolume/app/src/main/java/com/tiooooo/barvolume/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.tiooooo.barvolume
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    companion object {
+        private const val STATE_RESULT = "resultt"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_calculate) {
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+
+            var isEmpty = false
+
+            if (inputLength.isEmpty()) {
+                isEmpty = true
+                edtLength.error = "Field ini tidak boleh kosong"
+            }
+
+            if (inputHeight.isEmpty()) {
+                isEmpty = true
+                edtHeight.error = "Field ini tidak boleh kosong"
+            }
+
+            if (inputWidth.isEmpty()) {
+                isEmpty = true
+                edtWidth.error = "Field ini tidak boleh kosong"
+            }
+
+            if (!isEmpty) {
+                val volume = inputLength.toDouble() * inputHeight.toDouble() * inputWidth.toDouble()
+                tvResult.text = volume.toString()
+            }
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+}
